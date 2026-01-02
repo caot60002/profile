f95c6103d2236ab72bffa026574070a60c12287f
caot60002 <caot60002@gmail.com>
2026-01-02 18:12:35 +0700

    Create config.ts

---
+++ config.ts
@@ -0,0 +1,6 @@
+// CẤU HÌNH CÔNG KHAI (PUBLIC CONFIGURATION)
+// Để mọi người vào web là thấy ngay profile của bạn, hãy điền thông tin vào đây.
+// Bạn lấy các thông tin này từ bảng "Deployment Info" trong Editor sau khi đăng nhập Admin.
+
+export const PUBLIC_BIN_ID = "6957a6f743b1c97be91428d2"; // Ví dụ: "65c28d..."
+export const PUBLIC_API_KEY = "$2a$10$mMVm7AJIOBnIBwDqRjGv/eU/.AD26uDSyLTbWgxJrs1YpZaj2xH3m"; // Ví dụ: "$2a$10$..." (Lưu ý: Key này sẽ hiển thị trong mã nguồn, cân nhắc dùng Access Key nếu cần bảo mật cao)
